ce763d69c8f9922b48cc06ba1b9bad047f19250f
lina-stack <linabiri18072007@gmail.com>
2025-04-27 15:51:41 +0200

    Add files via upload

--- cadrillage.java
+++ cadrillage.java
@@ -1,40 +1,30 @@
 import java.awt.Color;
 import java.awt.*;
 import javax.swing.*;
-
+import java.lang.Thread;
+import java.io.File;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.ImageObserver;
 public class cadrillage {
     public static void main(String[] args) {
         MyComponent component = new MyComponent();
         JFrame frame = new JFrame();
-        frame.setSize(component.cell*component.col +110, component.cell*component.ligne+146); 
+        frame.setSize(component.cell*component.col +57, component.cell*component.ligne+140); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       
         frame.add(component);
         frame.setVisible(true);
         Graphics g = component.getGraphics();
         jeu j = new jeu(component.col,component.ligne);
        for(int i=0 ; i<5; i++){
-         j.coups();
-         if (j.X>=0&&j.Y>=0&&j.X<j.tab.length&&j.Y<j.tab[0].length&&j.tab[j.X][j.Y]==0) {
-            component.pieces(g, j.X, j.Y , j.trd );
-           }
-            if (j.X<0||j.Y<0||j.X>=j.tab.length||j.Y>=j.tab[0].length) {
-                component.rempli(g,"coup illegal! ");
-                j.depasse();
-                if (!(j.X<0||j.Y<0||j.X>=j.tab.length||j.Y>=j.tab[0].length)) {
-                    if (j.tab[j.X][j.Y]!=0) {
-                        component.rempli(g,"Deja rempli! ");
-                        j.rempli();
-                        if (j.tab[j.X][j.Y]!=0) {
-                            component.rempli(g,"mauvais coup tour perdu!");
-                        }else{
-                            component.pieces(g, j.X, j.Y , j.trd );
-                        }
-                    }else{component.pieces(g, j.X, j.Y , j.trd );}
-                   
-                }else{component.rempli(g,"mauvais coup tour perdu!");}
-            } 
-            component.pieces(g, j.X, j.Y , j.trd ); 
+        boolean valid  = j.coups();
+        System.out.println(valid);
+        if (valid) {
+           component.pieces(g, j.X, j.Y , j.trd );
+        }else{
+          component.non_valid(g ,"coup invalide!");
+        }
+       j.affiche_tab();
        j.trd=3-j.trd;
        }
 
@@ -47,40 +37,94 @@ class MyComponent extends JComponent {
       super.paintComponent(g);
       Graphics2D g2 = (Graphics2D) g;
       
-
-      Color brown = new Color(226, 178, 4 ,180);
-      g.setColor(brown);
-      g.fillRect(0, 0, cell * col + 110, cell * ligne + 110);
-
-      g.setColor(Color.BLACK);
-      
-       
-      for (int i = 0; i <= ligne; i++) {
-          g2.drawLine(50, i * cell + 50, col * cell + 50, i * cell + 50);
-      }
-      for (int i = 0; i <= col; i++) {
-          g2.drawLine(i * cell + 50, 50, i * cell + 50, ligne * cell + 50);
-      }  
-  }
-  public  void pieces(Graphics g , int x , int y , int trd) {
-    Graphics2D g2 = (Graphics2D) g;
-    if (trd == 1) g.setColor(Color.BLACK);
-    else g.setColor(Color.WHITE);
-    g.fillOval(x *cell+50+5, y *cell+50 +5, cell-10, cell-10);
-  }
-  public  void rempli(Graphics g  ,String text) {
-    Graphics2D g2 = (Graphics2D) g;
-    g.drawString(text, 250/2,250/2);
-    for (int i = 0; i < 20; i++) { // Réduire progressivement l'opacité
-        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, i/10));
-        g2.setColor(Color.BLACK);
-        g2.drawString(text, 125, 125);
+      Color boisFonce = new Color(190, 150, 110); // Cases foncées
+Color boisClair = new Color(235, 215, 190); // Cases claires
+Color fond = new Color(170, 130, 90 ,120);       // Fond
+Color bande = new Color(140, 100, 60 ,120);
 
         try {
-            Thread.sleep(100); // Pause pour voir l'effet progressivement
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+          Font font = Font.createFont(Font.TRUETYPE_FONT, new File("Bestime.ttf")).deriveFont(20f);
+          g2.setFont(font); // Appliquer la police une seule fois
+      } catch (Exception e) {
+          e.printStackTrace();
+          g2.setFont(new Font("Serif", Font.PLAIN, 30)); // Police par défaut si erreur
+      }
+       
+
+      g.setColor(fond);
+      g.fillRect(0, 0, cell * col + 43, cell * ligne + 110);
+      g.setColor(bande);
+      g.fillRect(0, 0, cell * col + 43, 60);
+      g.setColor(Color.BLACK);
+
+      ImageIcon icon = new ImageIcon("Image3.png");
+        Image image = icon.getImage();
+        g2.drawImage(image, 2, 2, 60, 60, this);
+
+
+      ImageIcon icon2 = new ImageIcon("Image6.png");
+        Image image2 = icon2.getImage();
+        g2.drawImage(image2, col*cell-50, 2, 60, 60, this);
+      boolean noir =true ;  
+      for (int y = 0; y < col; y++) {
+        for (int x = 0; x < ligne; x++) {
+            if (noir) {
+                g.setColor(boisFonce);
+            } else {
+                g.setColor(boisClair);
+            }
+            g.fillRect(x * cell+20, y * cell+80, cell, cell);
+            noir = !noir; // alterner la couleur
         }
+        noir = !noir; // alterner au début de chaque ligne
+        
+    }
+    points(g2, 1, 2);
+} 
+  
+  public  void pieces(Graphics g , int x , int y , int trd ) {
+    Graphics2D g2 = (Graphics2D) g;
+      if (trd == 1) g.setColor(Color.BLACK);
+      else g.setColor(Color.WHITE);
+      g.fillOval(x *cell+20+5, y *cell+80 +5, cell-10, cell-10);
+  
 }
+public void non_valid(Graphics g, String text) {
+  Graphics2D g2 = (Graphics2D) g;
+  try {
+      Font font = Font.createFont(Font.TRUETYPE_FONT, new File("Bestime.ttf")).deriveFont(20f);
+      g2.setFont(font); // Appliquer la police une seule fois
+  } catch (Exception e) {
+      e.printStackTrace();
+      g2.setFont(new Font("Serif", Font.PLAIN, 30)); // Police par défaut si erreur
+  }
+
+  int steps = 20; 
+  for (int i = 0; i <= steps; i++) {
+      float ratio = (float) i / steps;
+
+      int red   = (int) (174 * ratio);
+      int green = (int) (144 * ratio);
+      int blue  = (int) (117  * ratio);
+
+      g2.setColor(new Color(red, green, blue));
+      g2.drawString(text, cell*col/2-55, 35);
+      try {
+          Thread.sleep(70);
+      } catch (InterruptedException e) {
+          e.printStackTrace();
+      }
+  }
+  g2.setColor(new Color(174, 144, 117));
+  g2.drawString(text, cell*col/2-55, 35);
 }
+ public void points (Graphics g2, int x , int y ) {
+    g2.setColor(Color.black);
+        g2.drawString(String.valueOf(x), 50, 50);
+        g2.setColor(Color.white);
+        g2.drawString(String.valueOf(y), col*cell, 50);
+ } 
 }
+       
+
+
--- jeu.java
+++ jeu.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 public class jeu {
     int trd;
@@ -5,43 +7,107 @@ public class jeu {
     int Y;
     int[][]tab;
     public static void main(String[] args) {
-       
+        jeu j = new jeu(10,10);
+        for(int i=0 ; i<10; i++){
+           j.coups();
+           j.affiche_tab();
+           j.trd=3-j.trd;
+           }
+    
         }
-    
-
-    
     public jeu (int ligne , int col ) {
         trd=1;
         tab = new int[ligne][col];
+ 
     }
-    public  void depasse() {
-       // if (X<0||Y<0||X>=tab.length||Y>=tab[0].length) {
-             String X_Case = JOptionPane.showInputDialog("ce coup nest pas valide :entrez la coordonne X de la case x<"+X);
-            X = Integer.valueOf(X_Case);
-            String Y_Case = JOptionPane.showInputDialog("le coup precedent  nest pas valide: entrez al coordonne Y de la case y<"+Y);
-            Y = Integer.valueOf(Y_Case);
-            if (X>=0&&Y>=0&&X<tab.length&&Y<tab[0].length) {
-                tab[Y][X]=trd;
+    
+
+public  boolean  valid () {
+   
+    boolean c1=true ;
+    boolean c2=true ;
+    boolean c3=true ;
+    if (X<0||Y<0||X>=tab.length||Y>=tab[0].length) {
+        c1=false;
+        }else{
+            int [][] grp= group(X, Y, 0);
+            if(tab[X][Y]!=0){
+                c2=false ;
         
+            }
+            if(count_grp(grp)<10){
+                c3=false ;
+            }
+        }
+    
+     return c1&&c2&&c3 ;
+    }
+public int count_grp(int [][]tab) {
+    int somme =0 ;
+    for(int i=0 ; i<tab.length ; i++){
+        for(int j=0 ; j<tab[0].length ; j++){
+        if (tab[i][j]==1){
+            somme++;
+        }
+        }
+    }
+    return somme ;
+}             
+
+public int [][] group (int x ,int y, int  a) {
+    int ctr=0;
+   int [][]tab= new int[this.tab.length][this.tab[0].length];
+   tab[x][y]=1;
+   int trd =1;
+    boolean group =true;
+    while(group){
+        group = false ;
+    for(int i=0 ; i<tab.length ;i++){
+        for(int j=0 ; j<tab.length ;j++){
+            if(tab[i][j]==1){
+            if(i+1<tab.length){
+            if ((this.tab[i+1][j]==trd||this.tab[i+1][j]==a) &&(tab[i+1][j]==0)) {
+                tab[i+1][j]=trd;
+                group = true ;
+            } }
+            if (i-1>=0) {
+            if ((this.tab[i-1][j]==trd||this.tab[i-1][j]==a)&&(tab[i-1][j]==0)) {
+                tab[i-1][j]=trd;
+                group=true ;
+            }}
+            if (j+1<tab.length) {
+            if ((this.tab[i][j+1]==trd||this.tab[i][j+1]==a)&&(tab[i][j+1]==0)) {
+                tab[i][j+1]=trd;
+                group=true ;
+            }}
+            if (j-1>=0) {
+            if ((this.tab[i][j-1]==trd||this.tab[i][j-1]==a)&&(tab[i][j-1]==0)) {
+                tab[i][j-1]=trd;
+                group=true ;
+            }}
+        }
+           
+        }
     }
 }
-public  void  rempli () {
-   String X_Case = JOptionPane.showInputDialog("entrez une autre case 'X' que"+X);
-                X = Integer.valueOf(X_Case);
-               String Y_Case = JOptionPane.showInputDialog("entrez une autre case 'Y' que"+Y);
-                Y = Integer.valueOf(Y_Case);
-            if (tab[X][Y]==0) {
-                tab[Y][X]=trd;}
-                
+return tab ;
 }
-    public  void coups() { 
-        String X_Case = JOptionPane.showInputDialog("entrez al coordonne X de la case");
+    public boolean coups() { 
+        String X_Case = JOptionPane.showInputDialog("entrez la coordonne X de la case avec X<"+tab.length);
         X = Integer.valueOf(X_Case);
-        String Y_Case = JOptionPane.showInputDialog("entrez al coordonne Y de la case");
+        String Y_Case = JOptionPane.showInputDialog("entrez la coordonne Y de la case Y<"+tab.length);
         Y = Integer.valueOf(Y_Case);
-        if (X>=0&&Y>=0&&X<tab.length&&Y<tab[0].length) {
-            if(tab[X][Y]==0){
-            tab[Y][X]=trd;}}
+        if (valid()) {
+            tab[X][Y]=trd;
+            return true ;
+           }
+           else{
+            System.out.println("ce coup nest pas valide");
+            return false;
+        }
+    }
+
+public  void affiche_tab() {
     for(int i=0 ;i<tab.length; i++){
         for(int j=0 ;j<tab[i].length;j++){
             System.out.print(tab[i][j]);
